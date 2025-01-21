8a73e4c7f11b766b2924ab8fa88f7b379b376feb
akkisavi <akshaygosavi621@gmail.com>
2025-01-21 14:44:25 +0530

    reverted url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-// export const axiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "development"
-//       ? "http://localhost:5000/api"
-//       : "/api",
-// });
-
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "https://localhost:5000/api",
+      ? "http://localhost:5000/api"
+      : "/api",
 });
